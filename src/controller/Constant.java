93a2fbdc8d1a822a7a508677eb73ca7219a2e5e7
namthanh <nam.khuatthanh@powergatesoftware.net>
2021-10-10 01:07:15 +0700

    update iml

--- src/controller/Constant.java
+++ src/controller/Constant.java
@@ -1,8 +1,8 @@
 package controller;
 
 public class Constant {
-   public final static String DBURL = "jdbc:mysql://localhost:3306/ltmquiz";
-   public final static String USER = "nam";
+   public final static String DBURL = "jdbc:mysql://34.87.150.119:3306/ltmquiz";
+   public final static String USER = "root";
    public final static String PASSWORD = "123456";
 
    public final static Integer SERVERPORT = 1234;
